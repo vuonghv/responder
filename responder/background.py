4b44aadec1da8d563f53dd4092c2c2d68f8b8765
Vuong Hoang <vuonghv.cs@gmail.com>
2018-10-23 18:07:34 +0700

    Add callback to background.task
    Add on_success and on_exception callbacks when
    decorating background task

--- responder/background.py
+++ responder/background.py
@@ -1,3 +1,4 @@
+import traceback
 import multiprocessing
 import concurrent.futures
 
@@ -19,9 +20,24 @@ class BackgroundQueue:
         self.results.append(f)
         return f
 
-    def task(self, f):
-        def do_task(*args, **kwargs):
-            result = self.run(f, *args, **kwargs)
-            return result
+    def task(self, on_success=None, on_exception=None):
 
-        return do_task
+        def done_callback(fs):
+            try:
+                res = fs.result()
+                if on_success:
+                    on_success(res)
+            except Exception as error:
+                if on_exception:
+                    on_exception(error)
+                else:
+                    traceback.print_exc()
+
+        def decorator_do_task(f):
+            def do_task(*args, **kwargs):
+                result = self.run(f, *args, **kwargs)
+                result.add_done_callback(done_callback)
+                return result
+
+            return do_task
+        return decorator_do_task
